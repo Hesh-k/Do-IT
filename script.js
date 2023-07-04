134398b4db6264832caff27f11daf43457c5c566
IT22589804 <it22589804@my.sliit.lk>
2023-07-04 10:06:45 +0530

    dynamic content added

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 function addTask() {
-    var taskInput = document.getElementById('taskInput');
-    var taskList = document.getElementById('taskList');
+  var taskInput = document.getElementById('taskInput');
+  var taskList = document.getElementById('taskList');
+
+  // Create a new task item container
+  var newTask = document.createElement('div');
+  newTask.classList.add('task-item');
+
+  var itemText = document.createElement("span");
+  itemText.textContent = taskInput.value;
+
+  var deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete-button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", function() {
+    newTask.remove();
+  });
+
+  // Append the item text and delete button to the task item container
+  newTask.appendChild(itemText);
+  newTask.appendChild(deleteButton);
+
+  // Append the new task to the task list
+  taskList.appendChild(newTask);
+
+  // Clear the input field
+  taskInput.value = '';
+}
+
   
-    // Create a new list item
-    var newTask = document.createElement('li');
-    newTask.textContent = taskInput.value;
-  
-    // Add the new task to the task list
-    taskList.appendChild(newTask);
-  
-    // Clear the input field
-    taskInput.value = '';
-  }
-  
+
